47aed0bea44610db31c6ff5925682ea9035a8cf6
cyfotyasi <cyfotyasi@gmail.com>
2020-12-12 01:33:48 +0200

    basic functions

---
+++ src/main/kotlin/FirstClass.kt
@@ -0,0 +1,63 @@
+
+//val name: String = "Michael"
+//var surname: String? ="Tyasi"
+//var greeting : String ="hello"
+//
+//var gama: String? = null
+
+
+//fun getGreeting() = "Hello Kotlin"
+//
+//fun sayHello(): Unit{
+//    println(getGreeting())
+//}
+
+fun sayHello(itemToGreet:String) = println("Hello $itemToGreet")
+
+
+//fun sayHello(itemToGreet:String){
+//    val msg ="Molo  $itemToGreet"
+//    println(msg)
+//    println("Hello Kotlin")
+//}
+
+
+fun main(){
+
+sayHello(itemToGreet = "Cyfo")
+
+
+
+
+//    println("Hello World ")
+//    println(getGreeting())
+//    sayHello()
+
+//   greeting ="hi"
+//
+//    if(gama != null)
+//    {
+//        println(greeting)
+//    }else{
+//        println(greeting)
+//    }
+
+//    when(gama){
+//        null -> println("Hello")
+//        else -> println(greeting)
+//    }
+
+//    greeting = "hello"
+//    val greetingToprint = if(greeting !=null) greeting else "Hi"
+//
+
+
+
+
+
+//  println(name)
+//
+
+
+
+}
