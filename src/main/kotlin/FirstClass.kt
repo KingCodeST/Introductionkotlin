b6cf3f3979c20853d2df9aecb591ceb92d0a7e44
cyfotyasi <cyfotyasi@gmail.com>
2020-12-15 00:40:31 +0200

    basic functions

--- src/main/kotlin/FirstClass.kt
+++ src/main/kotlin/FirstClass.kt
@@ -12,7 +12,12 @@
 //    println(getGreeting())
 //}
 
-fun sayHello(itemToGreet:String) = println("Hello $itemToGreet")
+fun sayHello(greeting:String,vararg itemToGreet:String)
+{
+    itemToGreet.forEach{ itemToGreet ->
+        println("$greeting $itemToGreet")
+    }
+}
 
 
 //fun sayHello(itemToGreet:String){
@@ -21,12 +26,20 @@ fun sayHello(itemToGreet:String) = println("Hello $itemToGreet")
 //    println("Hello Kotlin")
 //}
 
+fun greetPerson(greeting: String ="Hello", name:String ="Kotlin"){
+    println("$greeting $name")
+}
 
 fun main(){
 
-sayHello(itemToGreet = "Cyfo")
+//    sayHello(greeting = "Hey" ,itemToGreet = "Cyfo")
+//    sayHello(greeting="Hello",itemToGreet = "World")
+
+    val interestingThings = arrayOf("Kotiln","Programming","Comic Books")
+    sayHello(greeting="Hi", *interestingThings)
 
 
+greetPerson()
 
 
 //    println("Hello World ")
@@ -59,5 +72,44 @@ sayHello(itemToGreet = "Cyfo")
 //
 
 
+//    val intrestingThings = arrayOf("Kotlin","Programing","Comic Books")
+//
+//
+//
+//    println(intrestingThings.size)
+//    println(intrestingThings[0])
+//    println(intrestingThings.get(0))
+//    println("")
+
+//    for(intrestringThing in intrestingThings)
+//    {
+//        println(intrestringThing)
+//    }
+
+//    intrestingThings.forEach {
+//        println(it)
+//    }
+
+
+//    intrestingThings.forEachIndexed{index, intrestingThing ->
+//        println("$intrestingThing is at index $index")
+//    }
+
+
+//    val users= listOf("Machael","Sdudla","Vuyisa")
+//    users.forEach{ user ->
+//        println(user)
+//    }
+
+//    sayHello(greeting = "Hi",)
+//
+//    val map = mapOf(1 to "a", 2 to "b", 3 to "c")
+//
+//    map.forEach{ key, value ->
+//        println("$key -> $value")
+//    }
+
+
 
 }
+
